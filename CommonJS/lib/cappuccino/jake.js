52ea472d9959353e4bbceb176ce290b2dc1d48dc
Johan Stille <johan@deltaprojects.com>
2015-10-29 13:18:20 +0100

    Removed blendtask.js and disabled NativeHost

--- CommonJS/lib/cappuccino/jake.js
+++ CommonJS/lib/cappuccino/jake.js
@@ -14,10 +14,10 @@ function exposeExports(path)
 exposeExports("objective-j/jake");
 
 // This check is only necessary because during the build process blendtask gets created much later.
-if (FILE.exists(FILE.join(FILE.dirname(module.path), "jake", "blendtask.j")))
-{
-    var BLEND_TASK = require("cappuccino/jake/blendtask");
+// if (FILE.exists(FILE.join(FILE.dirname(module.path), "jake", "blendtask.j")))
+// {
+//     var BLEND_TASK = require("cappuccino/jake/blendtask");
 
-    exports.BlendTask = BLEND_TASK.BlendTask;
-    exports.blend = BLEND_TASK.blend;
-}
+//     exports.BlendTask = BLEND_TASK.BlendTask;
+//     exports.blend = BLEND_TASK.blend;
+// }
